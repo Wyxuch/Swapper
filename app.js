0b9865fb69f0de501a33bd70903a17ee0098a9d6
Wyxuch <stefankieubasa@gmail.com>
2019-10-27 18:54:03 +0100

    changed input from uint8 to hex

--- app.js
+++ app.js
@@ -11,7 +11,12 @@ $('.exitButton').on('click',() => {win.close()})
 
 $('.minimizeButton').on('click',() => {win.minimize()})
 
-$('.saveButton').on('click', () => {modifyFiles()})
+$('.saveButton').on('click', () => {
+    let result = confirm("Do you really want to make changes?");
+    if(result){
+        modifyFiles()
+    }
+})
 
 $('.extensionInput').on('change', () => {
     extension = $('.extensionInput').val()
@@ -24,14 +29,6 @@ $('#choosePath').on('change',(e) => {
     pathInput = document.getElementById("choosePath").files[0].path
     if (extension != undefined){
         findFiles()
-    }
-})
-
-$('.Text').keypress((e) => {
-    if((e.charCode <= 57 && e.charCode >= 48) || e.charCode === 8 || e.charCode === 32){
-        return true
-    }else{
-        return false
     }
 })
 
@@ -76,15 +73,22 @@ function convertFiles(filePath){
 }
 
 function getInput(data, filePath){
-    const sequenceToChangeFrom = $('.binaryPrevious').val().split(' ')
-    const sequenceToChangeTo = $('.binaryChanged').val().split(' ')
+    const sequenceToChangeFrom = $('.binaryPrevious').val().replace(/\s/g, '')
+    const sequenceToChangeTo = $('.binaryChanged').val().replace(/\s/g, '')
+    console.log(sequenceToChangeFrom)
+    console.log(sequenceToChangeTo)
 
-    findSequence(data, sequenceToChangeFrom, sequenceToChangeTo, filePath)
+    const fromSequence = changeToUint8(sequenceToChangeFrom)
+    const toSequence = changeToUint8(sequenceToChangeTo)
+    console.log(fromSequence)
+    console.log(toSequence)
+
+    findSequence(data, fromSequence, toSequence, filePath)
 }
 
 function findSequence(initialData, fromSequence, toSequence, filePath){
     for(let i = 0; i < initialData.length; i++){
-        if (fromSequence[0] == initialData[i]){ //znajduje pierwszą liczbę
+        if (fromSequence[0] == initialData[i]){
             for(let j = 0; j < fromSequence.length; j++){
                 if (fromSequence[j] == initialData[i+j]){
                     if ((j + 1) == fromSequence.length && fromSequence[j] == initialData[i+j]){
@@ -128,4 +132,8 @@ function foundFullSequence(initialData, fromSequence, toSequence, i, filePath){
                 console.log(err)
             })
     }
+}
+
+function changeToUint8 (string){
+    return Uint8Array.from(Buffer.from(string, 'hex'))
 }
